3c5d4ed2df09623ef35de06cbf9ee50de6c1ed94
Anirudh <anirudhdagar6@gmail.com>
2021-03-09 15:35:57 +0530

    Fix #1: added card function

--- content.js
+++ content.js
@@ -102,43 +102,28 @@ async function unfollowers_list(user, unfollowed_by){
   localStorage.setItem(user + "_unfollowers", JSON.stringify(old_unfollowers));
 }
 
+function addNotificationToDOM_v2(dict2, dict1) {
 
-function addNotificationToDOM_v2(user1, user2) {
-  var username1 = user1["login"];
-  var username2 = user2["login"];
-
+  // let current_user = document.querySelector("body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-md-flex > details > details-menu > div.header-nav-current-user.css-truncate > a > strong").innerText;
   let current_user = document.querySelector("span.css-truncate.css-truncate-target.ml-1").innerText;
-  console.log(current_user);
-  let userU; let userO; let flag = 0;
-  if(username1==current_user || username2==current_user){
+  // console.log(current_user);
+  let flag = 0;
+  if(dict2['login']==current_user){
     flag = 1;
-    if(username1==current_user){
-      userU = username1;
-      userO = username2;
-    }
-    else{
-      userU = username2;
-      userO = username1;
-    }
-  }
-  else{
-    userU = username1;
-    userO = username2;
   }
 
   const divc = document.createElement("div");
   divc.classList.add("follow", "d-flex", "flex-items-baseline", "border-bottom", "color-border-secondary" ,"py-3");
 
-
   const card_spanDOM = document.createElement("span");
   card_spanDOM.classList.add("mr-3", "d-flex");
   const card_aDOM = document.createElement("a");
   card_aDOM.classList.add("d-inline-block");
-  card_aDOM.href = `/${userU}`;
+  card_aDOM.href = `/${dict1["login"]}`;
 
   const card_imageDOM = document.createElement("img");
   card_imageDOM.classList.add("avatar", "avatar-user");
-  card_imageDOM.src = user1["avatar_url"];
+  card_imageDOM.src = dict1["avatar_url"];
   card_imageDOM.width = "32";
   card_imageDOM.height = "32";
 
@@ -153,73 +138,103 @@ function addNotificationToDOM_v2(user1, user2) {
   const card_div3a = document.createElement("a");
   const card_div3b = document.createElement("a");
 
-
-  // Create info card
-  const divbox = document.createElement("div");
-  divbox.classList.add("Box", "p-3", "mt-2");
-  const divbox_content = document.createElement("div");
-  divbox_content.classList.add("d-flex");
-  const divbox_content_a = document.createElement("a");
-  divbox_content_a.title=`${userO}`;
-  divbox_content_a.href=`/${userO}`;
-
-  const box_imageDOM = document.createElement("img");
-  box_imageDOM.classList.add("avatar", "avatar-user", "mr-2");
-  box_imageDOM.src = user1["avatar_url"];
-  box_imageDOM.width = "40";
-  box_imageDOM.height = "40";
-  box_imageDOM.alt = `${userO}`;
-
-  const divbox_inner_content = document.createElement("div");
-  divbox_inner_content.classList.add("width-full", "ml-1");
-
-  const span_box = document.createElement("span");
-  span_box.classList.add("lh-condensed", "color-text-primary");
-
-  const span_box_inner = document.createElement("span");
-  span_box_inner.classList.add("mr-1");
-
-  const span_box_inner_a = document.createElement("a");
-  span_box_inner_a.classList.add("f5", "Link--secondary", "no-underline");
-  span_box_inner_a.href = `/${userO}`;
-
-  const div_bio = document.createElement("div");
-  div_bio.classList.add("dashboard-break-word", "lh-condensed", "mb-2", "mt-1", "bio")
-  const div_bio_text = document.createElement("div");
-  div_bio_text.innerText = `${user1["bio"]}`;
-
-  divbox_content_a.title=`${userO}`;
-  divbox_content_a.href=`/${userO}`;
-
   card_div3a.class="Link--primary no-underline text-bold wb-break-all d-inline-block";
   card_div3a.style="color:black; padding-right:3px; font-Weight:Bold";
 
   if(flag==1){
-    card_div3a.href=`/${userO}`;
-    card_div3a.innerText = ` ${userO}`;
+    card_div3a.href=`/${dict1["login"]}`;
+    card_div3a.innerText = ` ${dict1["login"]}`;
     card_div2.innerText = "unfollowed you";
   }
   else{
     card_div3b.class="Link--primary no-underline text-bold wb-break-all d-inline-block";
     card_div3b.style="color:black; padding-left:3px; font-Weight:Bold";
-    card_div3a.href=`/${userO}`;
-    card_div3a.innerText = ` ${userU}`;
+    card_div3a.href=`/${dict1["login"]}`;
+    card_div3a.innerText = ` ${dict1["login"]}`;
     card_div2.innerText = "unfollowed ";
-    card_div3b.href=`/${userU}`;
-    card_div3b.innerText = ` ${userO}`;
+    card_div3b.href=`/${dict2["login"]}`;
+    card_div3b.innerText = ` ${dict2["login"]}`;
   }
-
+  
   card_div3.append(card_div3a);
   card_div2.prepend(card_div3);
   card_div2.append(card_div3b);
-  card_div1.append(card_div2);
+  // card_div1.append(card_div2);
 
-  div_bio.append(div_bio_text);
-  divbox_inner_content.append(div_bio);
-  divbox_content.append(divbox_inner_content);
-  divbox.append(divbox_content);
+  //////////////////////////////////////////////
+  const divB = document.createElement("div");
+  divB.classList.add("follow", "Box", "p-3", "mt-2");
 
-  divc.append(card_spanDOM, card_div1, divbox);
+  const div1 = document.createElement("div");
+  div1.classList.add("d-flex");
+
+  const div1a = document.createElement("a");
+  div1a.href = `/${dict2["login"]}`; div1a.title = `${dict2["login"]}`;
+
+  const div1ai = document.createElement("img");
+  div1ai.classList.add("avatar", "avatar-user");
+  div1ai.src = dict2["avatar_url"];
+  div1ai.width = "32";
+  div1ai.height = "32";
+
+  const div2 = document.createElement("div");
+  div2.classList.add("width-full", "ml-1");
+
+  const span1 = document.createElement("span");
+  span1.classList.add("lh-condensed", "color-text-primary");
+
+  const span2 = document.createElement("span");
+  span2.classList.add("mr-1");
+
+  const span2a1 = document.createElement("a");
+  span2a1.classList.add("f4", "text-bold", "Link--primary", "no-underline");
+  span2a1.href = `/${dict2["login"]}`;
+  span2a1.innerText = `${dict2["name"]}`; ///// add name
+  const span2a2 = document.createElement("a");
+  span2a2.classList.add("f5", "Link--secondary", "no-underline");
+  span2a2.href = `/${dict2["login"]}`;
+  span2a2.innerText = `${dict2["login"]}`;
+  span2a2.style="padding-left:6px";
+
+  const div3= document.createElement("div");
+  div3.classList.add("dashboard-break-word", "lh-condensed", "mb-2", "mt-1", "bio");
+
+  const div4= document.createElement("div");
+  let string = `${dict2["bio"]}` //// add bio;
+  let replacedStr = string.replace(/@(\w+)/g, '<a href="https://github.com/$1">@$1</a>');
+  div4.innerHTML = `${replacedStr}`; 
+  
+  const p1=document.createElement("p");
+  p1.classList.add("f6", "color-text-secondary", "m-0");
+
+  const p1s1=document.createElement("span");
+  p1s1.classList.add("mr-3");
+  const p1s1a=document.createElement("a");
+  p1s1a.classList.add("Link--muted");
+  p1s1a.href = `/${dict2["login"]}?tab=repositories`;
+  p1s1a.innerText =  `${dict2["public_repos"]} repositories`;
+
+  const p1s2=document.createElement("span");
+  p1s2.classList.add("mr-3");
+  const p1s2a=document.createElement("a");
+  p1s2a.classList.add("Link--muted");
+  p1s2a.href = `/${dict2["login"]}?tab=followers`;
+  p1s2a.innerText =  `${dict2["followers"]} followers`;
+
+  p1s1.append(p1s1a); p1s2.append(p1s2a);
+  p1.append(p1s1, p1s2);
+
+  div3.append(div4);
+  span2.append(span2a1, span2a2);
+  span1.append(span2);
+  div2.append(span1, div3, p1);
+  div1a.append(div1ai);
+  div1.append(div1a, div2);
+  divB.append(div1);
+  //////////////////////
+  
+  card_div1.append(card_div2, divB);
+  divc.append(card_spanDOM, card_div1);
 
   document.querySelector("#dashboard > div > div:nth-child(5)").prepend(divc);
 }
