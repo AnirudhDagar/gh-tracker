81a0da4060ceb01f81b1a13e2a0b4e0c1a352abd
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-03-27 18:24:45 +0530

    Feat: Automatically add user from login info using message passing

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
-import { add_user, fetch_user_data, fetch_followers, unfollowers_list, compare_followers } from './utils.js'
+import { add_user, compare_followers } from './utils.js'
 import { read_storage } from './helper.js'
 
 
 let debug = true; // Debugging On!
 console.log("Background running!");
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
+  if (request.current_user_add == 'true'){
+    add_user(request.user_name);
+  }
   if (request.todo == 'getData'){
     console.log("Requesting to sendMessage!");
     let users;
@@ -13,7 +16,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
       users = value;
       compare_followers(debug).then((ans) => {
         unfollowed_pairs = ans;
-        console.log("Logger aara:", users, unfollowed_pairs);
         sendResponse({ "type": "data",
                        "users":  users,
                        "compared_unfollowers":  unfollowed_pairs});
