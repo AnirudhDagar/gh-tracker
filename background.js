3b6650c08ce4b6407625c6de6a7f28856945664b
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-03-29 04:02:03 +0530

    Feat: Fix #2 Timestamp of notification delivery with retention

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 import { add_user, compare_followers } from './utils.js'
 import { read_storage } from './helper.js'
 
-
-let debug = true; // Debugging On!
+let debug = false; // Debugging On!
 console.log("Background running!");
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
   if (request.current_user_add == 'true'){
@@ -12,13 +11,17 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
     console.log("Requesting to sendMessage!");
     let users;
     let unfollowed_pairs;
+    let unfollowers_data;
     read_storage("users").then((value) => {
       users = value;
       compare_followers(debug).then((ans) => {
-        unfollowed_pairs = ans;
+        unfollowed_pairs = ans.new;
+        unfollowers_data = ans.total;
+        console.log("unfollowers_info", unfollowers_data)
         sendResponse({ "type": "data",
-                       "users":  users,
-                       "compared_unfollowers":  unfollowed_pairs});
+             "users":  users,
+             "compared_unfollowers":  unfollowed_pairs,
+             "unfollowers_info":  unfollowers_data});
       });
     });
   }
