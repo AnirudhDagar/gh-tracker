53045f13594695e782ea3fff56563861793e1951
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-04-04 02:45:14 +0530

    Add: Docs & cleanups

--- content.js
+++ content.js
@@ -37,14 +37,18 @@ async function main(){
 }
 
 
+/**
+ * Injects the unfollowers card to the activity tab on github feed.
+ * @param {DateTime Object} previous_date
+ */
 function timeDifference(previous_date) {
-  let current = new Date();
   let msPerMinute = 60 * 1000;
   let msPerHour = msPerMinute * 60;
   let msPerDay = msPerHour * 24;
   let msPerMonth = msPerDay * 30;
   let msPerYear = msPerDay * 365;
 
+  let current = new Date();
   let elapsed = current - previous_date;
 
   if (elapsed < msPerMinute) {
@@ -68,7 +72,13 @@ function timeDifference(previous_date) {
 }
 
 
-function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
+/**
+ * Injects the unfollowers card to the activity tab on github feed.
+ * @param {JSON object} user_dict1
+ * @param {JSON object} user_dict2
+ * @param {str} unfollow_event_time
+ */
+function addNotificationToDOM(user_dict1, user_dict2, unfollow_event_time) {
   // Get time stamp data
   let activity_div = document.querySelector("#dashboard > .news > div[data-repository-hovercards-enabled]:not([class])");
   if (activity_div == null) {
@@ -90,9 +100,9 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
   // Get current logged in user
   let current_user = document.querySelector("body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-md-flex > details > summary > img").alt.replace("@", "");
   console.log("Current User: ", current_user);
-  let flag = false;
-  if(dict2['login']==current_user){
-    flag = true;
+  let flag_curr_user = false;
+  if(user_dict2['login']==current_user){
+    flag_curr_user = true;
   }
 
   const div_follow = document.createElement("div");
@@ -107,11 +117,11 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
   card_spanDOM.classList.add("mr-3", "d-flex");
   const card_aDOM = document.createElement("a");
   card_aDOM.classList.add("d-inline-block");
-  card_aDOM.href = `/${dict1["login"]}`;
+  card_aDOM.href = `/${user_dict1["login"]}`;
 
   const card_imageDOM = document.createElement("img");
   card_imageDOM.classList.add("avatar", "avatar-user");
-  card_imageDOM.src = dict1["avatar_url"];
+  card_imageDOM.src = user_dict1["avatar_url"];
   card_imageDOM.width = "32";
   card_imageDOM.height = "32";
 
@@ -138,24 +148,24 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
   card_div3a.classList.add("Link--primary", "no-underline", "text-bold", "wb-break-all", "d-inline-block");
   card_div3a.style=`color:${color}; padding-right:3px; font-Weight:600!important`;
 
-  if(flag){
-    card_div3a.href=`/${dict1["login"]}`;
-    card_div3a.innerText = ` ${dict1["login"]}`;
+  if(flag_curr_user){
+    card_div3a.href=`/${user_dict1["login"]}`;
+    card_div3a.innerText = ` ${user_dict1["login"]}`;
     baseline_div.innerText = "unfollowed you";
   }
   else{
     card_div3b.classList.add("Link--primary", "no-underline", "text-bold", "wb-break-all", "d-inline-block");
     card_div3b.style=`color:${color}; padding-left:3px; font-Weight:600!important`;
-    card_div3a.href=`/${dict1["login"]}`;
-    card_div3a.innerText = ` ${dict1["login"]}`;
+    card_div3a.href=`/${user_dict1["login"]}`;
+    card_div3a.innerText = ` ${user_dict1["login"]}`;
     baseline_div.innerText = "unfollowed ";
-    card_div3b.href=`/${dict2["login"]}`;
-    card_div3b.innerText = ` ${dict2["login"]}`;
+    card_div3b.href=`/${user_dict2["login"]}`;
+    card_div3b.innerText = ` ${user_dict2["login"]}`;
   }
   
   card_div3.append(card_div3a);
   baseline_div.prepend(card_div3);
-  if (!flag){
+  if (!flag_curr_user){
     baseline_div.append(card_div3b);
   }
   span_date.append(relative_time_tag);
@@ -169,11 +179,11 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
   div1.classList.add("d-flex");
 
   const div1a = document.createElement("a");
-  div1a.href = `/${dict2["login"]}`; div1a.title = `${dict2["login"]}`;
+  div1a.href = `/${user_dict2["login"]}`; div1a.title = `${user_dict2["login"]}`;
 
   const div1ai = document.createElement("img");
   div1ai.classList.add("avatar", "avatar-user", "mr-2");
-  div1ai.src = dict2["avatar_url"];
+  div1ai.src = user_dict2["avatar_url"];
   div1ai.width = "40";
   div1ai.height = "40";
 
@@ -188,26 +198,26 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
 
   const span2a1 = document.createElement("a");
   span2a1.classList.add("f4", "text-bold", "Link--primary", "no-underline");
-  span2a1.href = `/${dict2["login"]}`;
-  let name = dict2["name"];
+  span2a1.href = `/${user_dict2["login"]}`;
+  let name = user_dict2["name"];
   if (name){
     span2a1.innerText = `${name}`; // add name
   }
   else{
-    name = dict2["login"];
+    name = user_dict2["login"];
     span2a1.innerText = `${name}`; // add login instead of name
   }
   const span2a2 = document.createElement("a");
   span2a2.classList.add("f5", "Link--secondary", "no-underline");
-  span2a2.href = `/${dict2["login"]}`;
-  span2a2.innerText = `${dict2["login"]}`;
+  span2a2.href = `/${user_dict2["login"]}`;
+  span2a2.innerText = `${user_dict2["login"]}`;
   span2a2.style = "padding-left:6px";
 
   const div3 = document.createElement("div");
   div3.classList.add("dashboard-break-word", "lh-condensed", "mb-2", "mt-1", "bio");
 
   const div4 = document.createElement("div");
-  bio_text = dict2["bio"]
+  bio_text = user_dict2["bio"]
   if (bio_text){
     let string = `${bio_text}`;  // add bio
     // Replace all text in bio to create @orgs links
@@ -226,15 +236,15 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
   p1s1.classList.add("mr-3");
   const p1s1a=document.createElement("a");
   p1s1a.classList.add("Link--muted");
-  p1s1a.href = `/${dict2["login"]}?tab=repositories`;
-  p1s1a.innerText =  `${dict2["public_repos"]} repositories`;
+  p1s1a.href = `/${user_dict2["login"]}?tab=repositories`;
+  p1s1a.innerText =  `${user_dict2["public_repos"]} repositories`;
 
   const p1s2=document.createElement("span");
   p1s2.classList.add("mr-3");
   const p1s2a=document.createElement("a");
   p1s2a.classList.add("Link--muted");
-  p1s2a.href = `/${dict2["login"]}?tab=followers`;
-  p1s2a.innerText =  `${dict2["followers"]} followers`;
+  p1s2a.href = `/${user_dict2["login"]}?tab=followers`;
+  p1s2a.innerText =  `${user_dict2["followers"]} followers`;
 
   p1s1.append(p1s1a); p1s2.append(p1s2a);
   p1.append(p1s1, p1s2);
@@ -250,8 +260,7 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
   div_body.append(div_card);
   div_follow.append(div_body);
 
-
-  // Now loop through thus event_lists comparing our current event time to put the card there.
+  // Now loop through this event_lists comparing our current event time to put the card there.
   let most_recent_event_time = new Date(event_lists[0].getAttribute("datetime"));
   if (unfollow_event_time > most_recent_event_time){
     console.log("Insert at the top!");
@@ -269,6 +278,7 @@ function addNotificationToDOM(dict1, dict2, unfollow_event_time) {
     }
   }
 }
+
 
 window.addEventListener('load', (event) => {
     console.log('The page has fully loaded');
